010f22728889afd697d9df55a9ec4eb4071f8789
Rdg1328 <57833035+Rdg1328@users.noreply.github.com>
2020-12-16 21:22:05 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+from Aluno import Aluno
+from AlunoGraduacao import AlunoGraduacao
+from AluEnsinoMedio import AluEnsinoMedio
+
+alu = Aluno('13', 'Rodrigo', '3981')
+alu1 = AlunoGraduacao('13', 'Rodrigo', '3981', '2')
+alu2 = AluEnsinoMedio('13', 'Rodrigo', '3981', '4')
+
+alu.Imprimir()
+alu1.Imprimir()
+alu2.Imprimir()
